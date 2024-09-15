9c560258ff55b6d4b0f64fb627238e9163af5c5d
abhishekbhonde <abhishekbhonde567@gmail.com>
2024-09-15 21:46:36 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 // backend/index.js
 const express = require('express');
+const helmet = require('helmet');
+
 const cors = require("cors");
 const rootRouter = require("./routes/index");
 
 const app = express();
 
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      fontSrc: ["'self'", "https://fonts.gstatic.com"],
+      styleSrc: ["'self'", "https://cdnjs.cloudflare.com"],
+      scriptSrc: ["'self'"]
+    }
+  }));
 app.use(cors(
     {
         origin:["https://cashless-wallet-v2-mkoa.vercel.app/"],
